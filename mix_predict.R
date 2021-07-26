6f8e168d8313df2db7c62ab74ae1bf87e2c07dea
unknown <gmandiola@azti.es>
2021-07-26 15:00:57 +0200

    los resultados de los modelos + clasificaciones de los datos mixtos

--- mix_predict.R
+++ mix_predict.R
@@ -7,7 +7,7 @@ names(datos)
 datos <- datos.sinna %>% 
   filter(sp %in% c("mix")) %>%
   select(-c(ref, time, z.038, z.070, z.120, z.200))
-#predict mixed data---- 
+#predict mixed data separating sets
 #using models created in the previous script
 #calculate probability of class, by model, for set----
 #separate set----
@@ -22,56 +22,132 @@ pre_svm69 = predict(svm_model, newdata = lance69, type = "prob")
 pre_gbm69 = predict(gbm_model, newdata = lance69, type = "prob")
 pre_rf69 = predict(rf_model, newdata = lance69, type = "prob")
 
-#percent of species by each set and model----
-skj_svm60 = nrow(filter(pre_svm60, skj > 0.8)) / nrow(pre_svm60) * 100
-yft_svm60 = nrow(filter(pre_svm60, skj < 0.2)) / nrow(pre_svm60) * 100
-other_svm60 = nrow(filter(pre_svm60, skj > 0.2, skj < 0.8)) / nrow(pre_svm60) * 100
-
-skj_gbm60 = nrow(filter(pre_gbm60, skj > 0.8)) / nrow(pre_gbm60) * 100
-yft_gbm60 = nrow(filter(pre_gbm60, skj < 0.2)) / nrow(pre_gbm60) * 100
-other_gbm60 = nrow(filter(pre_gbm60, skj > 0.2, skj < 0.8)) / nrow(pre_gbm60) * 100
-
-skj_rf60 = nrow(filter(pre_rf60, skj > 0.8)) / nrow(pre_rf60) * 100
-yft_rf60 = nrow(filter(pre_rf60, skj < 0.2)) / nrow(pre_rf60) * 100
-other_rf60 = nrow(filter(pre_rf60, skj > 0.2, skj < 0.8)) / nrow(pre_rf60) * 100
-
-skj_svm69 = nrow(filter(pre_svm69, skj > 0.8)) / nrow(pre_svm69) * 100
-yft_svm69 = nrow(filter(pre_svm69, skj < 0.2)) / nrow(pre_svm69) * 100
-other_svm69 = nrow(filter(pre_svm69, skj > 0.2, skj < 0.8)) / nrow(pre_svm69) * 100
-
-skj_gbm69 = nrow(filter(pre_gbm69, skj > 0.8)) / nrow(pre_gbm69) * 100
-yft_gbm69 = nrow(filter(pre_gbm69, skj < 0.2)) / nrow(pre_gbm69) * 100
-other_gbm69 = nrow(filter(pre_gbm69, skj > 0.2, skj < 0.8)) / nrow(pre_gbm69) * 100
-
-skj_rf69 = nrow(filter(pre_rf69, skj > 0.8)) / nrow(pre_rf69) * 100
-yft_rf69 = nrow(filter(pre_rf69, skj < 0.2)) / nrow(pre_rf69) * 100
-other_rf69 = nrow(filter(pre_rf69, skj > 0.2, skj < 0.8)) / nrow(pre_rf69) * 100
-
-#all prediction----
-resultados = data.frame(skj_svm60, yft_svm60, other_svm60, skj_gbm60, yft_gbm60, other_gbm60, skj_rf60, yft_rf60, other_rf60,
-           skj_svm69, yft_svm69, other_svm69, skj_gbm69, yft_gbm69, other_gbm69, skj_rf69, yft_rf69, other_rf69)
-
-
-accuracy_allmodels %>%
-  ggplot(aes(x = row.names()))+
-  geom_point()+
-  facet_wrap(~ accuracy_allmodels$modelo)
-
-predict(gbm_model, newdata = datos)
-predict(rf_model, newdata = datos)
-
-#compare mix bio data
+#compare mix bio data, datos de lances mixtos tidy----
 mix_data = read.table("datos\\mix_bio_data_tidy.csv", header = TRUE, sep = ",")
-
-str(mix_data)
-
-str(mix_data)
-if(mix_data$set == "60") {
-  skj60 = sum(mix_data$skj)/nrow()
+#porcentajes reales de cada especie en lances mixtos----
+#60
+mix_60_skj = mix_data %>%
+  filter(set == 60) %>%
+  filter(sp == "skj") %>%
+  nrow() / nrow(filter(mix_data, set == 60)) * 100
+mix_60_yft = mix_data %>%
+  filter(set == 60) %>%
+  filter(sp == "yft") %>%
+  nrow() / nrow(filter(mix_data, set == 60)) * 100
+mix_60_other = mix_data %>%
+  filter(set == 60) %>%
+  filter(sp == "other") %>%
+  nrow() / nrow(filter(mix_data, set == 60)) * 100
+#69
+mix_69_skj = mix_data %>%
+  filter(set == 69) %>%
+  filter(sp == "skj") %>%
+  nrow() / nrow(filter(mix_data, set == 69)) * 100
+mix_69_yft = mix_data %>%
+  filter(set == 69) %>%
+  filter(sp == "yft") %>%
+  nrow() / nrow(filter(mix_data, set == 69)) * 100
+mix_69_other = mix_data %>%
+  filter(set == 69) %>%
+  filter(sp == "other") %>%
+  nrow() / nrow(filter(mix_data, set == 69)) * 100
+cbind(mix_60_skj, mix_60_yft, mix_60_other, mix_69_skj, mix_69_yft, mix_69_other)
+#porcentaje de predicciones en cada modelo y lance----
+#primero asignamos valores de umbral, es decir, que probabilidades queremos que se consideren una especie para clasificarlas como tal
+umbral = c(0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.91, 0.95)
+for (i in 1:length(umbral)) {
+  skj_svm60[i] = nrow(filter(pre_svm60, skj > umbral[i])) / nrow(pre_svm60) * 100
+  skj_gbm60[i] = nrow(filter(pre_gbm60, skj > umbral[i])) / nrow(pre_gbm60) * 100
+  skj_rf60[i] = nrow(filter(pre_rf60, skj > umbral[i])) / nrow(pre_rf60) * 100
+  
+  skj_svm69[i] = nrow(filter(pre_svm69, skj > umbral[i])) / nrow(pre_svm69) * 100
+  skj_gbm69[i] = nrow(filter(pre_gbm69, skj > umbral[i])) / nrow(pre_gbm69) * 100
+  skj_rf69[i] = nrow(filter(pre_rf69, skj > umbral[i])) / nrow(pre_rf69) * 100
+  
+  yft_svm60[i] = nrow(filter(pre_svm60, yft > umbral[i])) / nrow(pre_svm60) * 100
+  yft_gbm60[i] = nrow(filter(pre_gbm60, yft > umbral[i])) / nrow(pre_gbm60) * 100
+  yft_rf60[i] = nrow(filter(pre_rf60, yft > umbral[i])) / nrow(pre_rf60) * 100
+  
+  yft_svm69[i] = nrow(filter(pre_svm69, yft > umbral[i])) / nrow(pre_svm69) * 100
+  yft_gbm69[i] = nrow(filter(pre_gbm69, yft > umbral[i])) / nrow(pre_gbm69) * 100
+  yft_rf69[i] = nrow(filter(pre_rf69, yft > umbral[i])) / nrow(pre_rf69) * 100
 }
-  group_by(set)
 
-group_by(mix_data, set)
-#tengo que conseguir el porcentaje de cada especie en los datos originales
+other_svm60 = 100-(skj_svm60 + yft_svm60)
+other_gbm60 = 100-(skj_gbm60 + yft_gbm60)
+other_rf60 = 100-(skj_rf60 + yft_rf60)
 
+other_svm69 = 100-(skj_svm69 + yft_svm69)
+other_gbm69 = 100-(skj_gbm69 + yft_gbm69)
+other_rf69 = 100-(skj_rf69 + yft_rf69)
 
+#tenemos los porcentajes obtenidos para cada especie dependiendo del valor de probabilidad asignado para cada uno. 
+#Es decir, para umbral "x", tenemos los porcentajes de cada especie dependiendo de modelo y lance.
+data.frame(cbind(umbral, skj_svm60, skj_gbm60 , skj_rf60, skj_svm69, skj_gbm69, skj_rf69, 
+      yft_svm60, yft_gbm60, yft_rf60, yft_svm69, yft_gbm69, yft_rf69,
+      other_svm60, other_gbm60, other_rf60, other_svm69, other_gbm69, other_rf69))
+
+#aciertos de porcentaje de especie en cada lance dependiendo de modelo.----
+#acierto = (%skjobtenido - %skjreal) / %skjreal
+(skj_svm60 - mix_60_skj)/mix_60_skj
+
+skjmix = data.frame(abs(cbind(skj_svm60, skj_gbm60 , skj_rf60, skj_svm69, skj_gbm69, skj_rf69) - mix_60_skj) / mix_60_skj)
+skjmix = cbind(umbral, skjmix)
+colnames(skjmix) = c("umbral", "svm 60", "gbm 60", "rf 60", "svm 69", "gbm 69", "rf 69")
+yftmix = data.frame(abs(cbind(yft_svm60, yft_gbm60, yft_rf60, yft_svm69, yft_gbm69, yft_rf69) - mix_60_yft) / mix_60_yft)
+yftmix = cbind(umbral, yftmix)
+colnames(yftmix) = c("umbral", "svm 60", "gbm 60", "rf 60", "svm 69", "gbm 69", "rf 69")
+othermix = data.frame(abs(cbind(other_svm60, other_gbm60, other_rf60, other_svm69, other_gbm69, other_rf69) - mix_60_other) / mix_60_other)
+othermix = cbind(umbral, othermix)
+colnames(othermix) = c("umbral", "svm 60", "gbm 60", "rf 60", "svm 69", "gbm 69", "rf 69")
+
+#hacer graficos con estos valores de acierto para ver que valor es el optimo para clasificar especies. el que nos da = 0.
+#Cuando haga el grafico elejir valro optimo.
+str(skjmix)
+aciertos = cbind(umbral, skjmix, yftmix, othermix)
+str(aciertos)
+aciertos %>%
+  pivot_longer(cols = 2:19)%>%
+  arrange(name, umbral, value)%>%
+  ggplot(aes(x = umbral, y = value, colour = name))+
+  geom_line()
+#umbral optimo por especie----
+
+skjmix %>%
+  pivot_longer(cols = 2:7)%>%
+  arrange(name, umbral, value)%>%
+  ggplot(aes(x = umbral, y = value, colour = name))+
+  geom_line() +
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="SKJ",
+       x="valor de umbral",
+       y="Acierto",
+       caption="Fuente: ",
+       colour = "modelo y lance")
+
+yftmix %>%
+  pivot_longer(cols = 2:7)%>%
+  arrange(name, umbral, value)%>%
+  ggplot(aes(x = umbral, y = value, colour = name))+
+  geom_line()+
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="YFT",
+       x="valor de umbral",
+       y="Acierto",
+       caption="Fuente: ",
+       colour = "modelo y lance")
+
+othermix %>%
+  pivot_longer(cols = 2:7)%>%
+  arrange(name, umbral, value)%>%
+  ggplot(aes(x = umbral, y = value, colour = name))+
+  geom_line()+
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="OTHER",
+       x="valor de umbral",
+       y="Acierto",
+       caption="Fuente: ",
+       colour = "modelo y lance")
--- results.R
+++ results.R
@@ -1,34 +1,45 @@
-#plot results
+library(tidyverse)
+library(caret)
+#**********************************************
+#primero vamos a ver los resultados de los modelos creados a principio
+#Valores de todas las repeticiones y particiones del modelo creado con cv y reduced data----
+metricas_resamples %>%
+  ggplot(aes(x = modelo, y = valor, fill = metrica)) +
+  geom_boxplot(alpha = 0.7) +
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="Accuracy and Kappa",
+       x="Method",
+       y="Value",
+       caption="Fuente: ")
+
+
+#ACCURACY----
+#**********************************************
 #crearemos un dataframe con todos los resultados----
 #1. datos reducidos y lances separados
-acc_svm_10_sep = cbind(rep("svm", each = 10), results_svm10_sep[, 1], rep("reduced", each = 10), rep("separated", each = 10))
+acc_svm_10_sep = cbind(rep("svm", each = 10), as.numeric(results_svm10_sep[, 1]), rep("reduced", each = 10), rep("separated", each = 10))
 acc_gbm_10_sep = cbind(rep("gbm", each = 10), results_gbm_10_sep[, 1], rep("reduced", each = 10), rep("separated", each = 10))
 acc_rf_10_sep = cbind(rep("rf", each = 10), results_rf10_sep[, 1], rep("reduced", each = 10), rep("separated", each = 10))
 #2. datos completos y lances separados
 acc_svm_all_sep = cbind(rep("svm", each = 10), results_svm_all_sep[, 1], rep("complete", each = 10), rep("separated", each = 10))
-acc_gbm_all_sep = cbind(rep("gbm", each = 10), results_gbm_all_sep[, 1], rep("complete", each = 10), rep("separatde", each = 10))
-acc_rf_all_sep = cbind(rep("rf", each = 10), results_rf_all_sep[, 1], rep("complete", each = 10), rep("separatde", each = 10))
+acc_gbm_all_sep = cbind(rep("gbm", each = 10), results_gbm_all_sep[, 1], rep("complete", each = 10), rep("separated", each = 10))
+acc_rf_all_sep = cbind(rep("rf", each = 10), results_rf_all_sep[, 1], rep("complete", each = 10), rep("separated", each = 10))
 #3. datos reducidos y lances unidos
 acc_svm_10_all = cbind(rep("svm", each = 25), accuracy_svm_10[, 4], rep("reduced", each = 25), rep("united", each = 25))
 acc_gbm_10_all = cbind(rep("gbm", each = 25), accuracy_gbm_10[, 4], rep("reduced", each = 25), rep("united", each = 25))
 acc_rf_10_all = cbind(rep("rf", each = 25), accuracy_rf_10[, 4], rep("reduced", each = 25), rep("united", each = 25))
 #4. datos completos (eliminando NA) y lances unidos
-acc_svm_all = cbind(rep("svm", each = 25), accuracy_svm_all[, 4], rep("completed_na_rm", each = 25), rep("united", each = 25))
-acc_gbm_all = cbind(rep("gbm", each = 25), accuracy_gbm_all[, 4], rep("completed_na_rm", each = 25), rep("united", each = 25))
-acc_rf_all = cbind(rep("rf", each = 25), accuracy_rf_all[, 4], rep("completed_na_rm", each = 25), rep("united", each = 25))
+acc_svm_all = cbind(rep("svm", each = 25), accuracy_svm_all[, 4], rep("complete", each = 25), rep("united_na_rm", each = 25))
+acc_gbm_all = cbind(rep("gbm", each = 25), accuracy_gbm_all[, 4], rep("complete", each = 25), rep("united_na_rm", each = 25))
+acc_rf_all = cbind(rep("rf", each = 25), accuracy_rf_all[, 4], rep("complete", each = 25), rep("united_na_rm", each = 25))
 #5. datos completos (importando NA) y lances unidos
-acc_svm_all_imp = cbind(rep("svm", each = 25), accuracy_svm_imp[, 4], rep("completed_na_imp", each = 25), rep("united", each = 25))
-acc_gbm_all_imp = cbind(rep("gbm", each = 25), accuracy_gbm_imp[, 4], rep("completed_na_imp", each = 25), rep("united", each = 25))
-acc_rf_all_imp = cbind(rep("rf", each = 25), accuracy_rf_imp[, 4], rep("completed_na_imp", each = 25), rep("united", each = 25))
+acc_svm_all_imp = cbind(rep("svm", each = 25), accuracy_svm_imp[, 4], rep("complete", each = 25), rep("united_na_imp", each = 25))
+acc_gbm_all_imp = cbind(rep("gbm", each = 25), accuracy_gbm_imp[, 4], rep("complete", each = 25), rep("united_na_imp", each = 25))
+acc_rf_all_imp = cbind(rep("rf", each = 25), accuracy_rf_imp[, 4], rep("complete", each = 25), rep("united_na_imp", each = 25))
 
 #dataframe con todos los resultados----
 #("modelo", "accuracy", "tipo de dato", "gestion de lances")
-resultados_tidy = rbind(acc_svm_10_sep, acc_gbm_10_sep, acc_rf_10_sep, acc_svm_all_sep, acc_svm_all_sep, acc_gbm_all_sep, acc_rf_all_sep,
-      acc_svm_10_all, acc_gbm_10_all, acc_rf_10_all, acc_svm_all, acc_gbm_all, acc_rf_all_imp, acc_svm_all_imp,
-      acc_gbm_all_imp, acc_rf_all_imp)
-colnames(resultados_tidy) = c("model", "accuracy", "data", "set")
-
-
 #dataframe de lances separados por modelo y tipo de dato usado----
 separados = data.frame(cbind(acc_svm_10_sep, acc_gbm_10_sep, acc_rf_10_sep, acc_svm_all_sep, acc_gbm_all_sep, acc_rf_all_sep))
 colnames(separados) = c("svm_reduced", "gbm_reduced", "rf_reduced", "svm_alldata", "gbm_alldata", "rf_alldata")
@@ -37,12 +48,199 @@ colnames(separados) = c("svm_reduced", "gbm_reduced", "rf_reduced", "svm_alldata
 unidos = data.frame(cbind(acc_svm_10_all, acc_gbm_10_all, acc_rf_10_all, acc_svm_all, acc_gbm_all, acc_rf_all, acc_svm_all_imp, acc_gbm_all_imp, acc_rf_all_imp))
 colnames(unidos) = c("svm_reduced", "gbm_reduced", "rf_reduced", "svm_nadeleted", "gbm_nadeleted", "rf_nadeleted", 
                      "svm_naimputed", "gbm_naimputed", "rf_naimputed")  
-#tidy results
+#resultados formato tidy----
+resultados_tidy = data.frame(rbind(acc_svm_10_sep, acc_gbm_10_sep, acc_rf_10_sep, acc_svm_all_sep, acc_svm_all_sep, acc_gbm_all_sep, acc_rf_all_sep,
+                        acc_svm_10_all, acc_gbm_10_all, acc_rf_10_all, acc_svm_all, acc_gbm_all, acc_rf_all, acc_svm_all_imp,
+                        acc_gbm_all_imp, acc_rf_all_imp)) 
+colnames(resultados_tidy) = c("model", "accuracy", "data", "set")
+#graficos de resultados----
+#datos reducidos, accuracy de modelos comparando lances unidos y separados
+#******************************
+g1 <- resultados_tidy %>%
+  filter(data == "reduced") %>%
+  ggplot(aes(x = model, y = as.numeric(accuracy), fill = set)) +
+  geom_boxplot(alpha = 0.7) +
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="Reduced data",
+       x="Method",
+       y="Accuracy",
+       caption="Fuente: ")
+g1 
+
+#datos completos, accuracy de modelos comparando lances unidos (eliminando NA y importando NA) y lances separados.
+g2 <- resultados_tidy %>%
+  filter(data == "complete") %>%
+  ggplot(aes(x = model, y = as.numeric(accuracy), fill = set)) +
+  geom_boxplot(alpha = 0.7) +
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="Complete data",
+       x="Method",
+       y="Accuracy",
+       caption="Fuente: ")
+g2 + scale_fill_discrete(labels=c("separated", "united, importing NA values", "united, removing NA values"))
+#************************************
+#datos unidos (eliminando NA), accuracy de modelos comparando datos reducidos y completos
+g3 <- resultados_tidy %>%
+  filter(set == c("united_na_rm", "united")) %>%
+  ggplot(aes(x = model, y = as.numeric(accuracy), fill = data)) +
+  geom_boxplot(alpha = 0.7) +
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="United data, removing NA values",
+       x="Method",
+       y="Accuracy")
+g3 
+
+#datos unidos (importando NA), accuracy de modelos comparando datos reducidos y completos
+g4 <- resultados_tidy %>%
+  filter(set == c("united_na_imp", "united")) %>%
+  ggplot(aes(x = model, y = as.numeric(accuracy), fill = data)) +
+  geom_boxplot(alpha = 0.7) +
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="United data, importing NA values
+       NA values",
+       x="Method",
+       y="Accuracy")
+g4 
+
+#datos separados, accuracy de modelos comparando datos reducidos y completos
+g5 <- resultados_tidy %>%
+  filter(set == c("separated")) %>%
+  ggplot(aes(x = model, y = as.numeric(accuracy), fill = data)) +
+  geom_boxplot(alpha = 0.7) +
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="Separated data, comparing reduced and completed data",
+       x="Method",
+       y="Accuracy")
+g5 
+#**********************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
   
-  
+
+#**********************************************
+#KAPPA----
+#**********************************************
+#crearemos un dataframe con todos los resultados----
+#1. datos reducidos y lances separados
+kp_svm_10_sep = cbind(rep("svm", each = 10), results_svm10_sep[, 2], rep("reduced", each = 10), rep("separated", each = 10))
+kp_gbm_10_sep = cbind(rep("gbm", each = 10), results_gbm_10_sep[, 2], rep("reduced", each = 10), rep("separated", each = 10))
+kp_rf_10_sep = cbind(rep("rf", each = 10), results_rf10_sep[, 2], rep("reduced", each = 10), rep("separated", each = 10))
+#2. datos completos y lances separados
+kp_svm_all_sep = cbind(rep("svm", each = 10), results_svm_all_sep[, 2], rep("complete", each = 10), rep("separated", each = 10))
+kp_gbm_all_sep = cbind(rep("gbm", each = 10), results_gbm_all_sep[, 2], rep("complete", each = 10), rep("separated", each = 10))
+kp_rf_all_sep = cbind(rep("rf", each = 10), results_rf_all_sep[, 2], rep("complete", each = 10), rep("separated", each = 10))
+#3. datos reducidos y lances unidos
+kp_svm_10_all = cbind(rep("svm", each = 25), kappa_svm_10[, 4], rep("reduced", each = 25), rep("united", each = 25))
+kp_gbm_10_all = cbind(rep("gbm", each = 25), kappa_gbm_10[, 4], rep("reduced", each = 25), rep("united", each = 25))
+kp_rf_10_all = cbind(rep("rf", each = 25), kappa_rf_10[, 4], rep("reduced", each = 25), rep("united", each = 25))
+#4. datos completos (eliminando NA) y lances unidos
+kp_svm_all = cbind(rep("svm", each = 25), kappa_svm_all[, 4], rep("complete", each = 25), rep("united_na_rm", each = 25))
+kp_gbm_all = cbind(rep("gbm", each = 25), kappa_gbm_all[, 4], rep("complete", each = 25), rep("united_na_rm", each = 25))
+kp_rf_all = cbind(rep("rf", each = 25), kappa_rf_all[, 4], rep("complete", each = 25), rep("united_na_rm", each = 25))
+#5. datos completos (importando NA) y lances unidos
+kp_svm_all_imp = cbind(rep("svm", each = 25), kappa_svm_imp[, 4], rep("complete", each = 25), rep("united_na_imp", each = 25))
+kp_gbm_all_imp = cbind(rep("gbm", each = 25), kappa_gbm_imp[, 4], rep("complete", each = 25), rep("united_na_imp", each = 25))
+kp_rf_all_imp = cbind(rep("rf", each = 25), kappa_rf_imp[, 4], rep("complete", each = 25), rep("united_na_imp", each = 25))
+
+#dataframe con todos los resultados----
+#("modelo", "kappa", "tipo de dato", "gestion de lances")
+#dataframe de lances separados por modelo y tipo de dato usado----
+separados_kp = data.frame(cbind(kp_svm_10_sep, kp_gbm_10_sep, kp_rf_10_sep, kp_svm_all_sep, kp_gbm_all_sep, kp_rf_all_sep))
+colnames(separados_kp) = c("svm_reduced", "gbm_reduced", "rf_reduced", "svm_alldata", "gbm_alldata", "rf_alldata")
+
+#data frame de lances unidos por modelo y tipo de dato----
+unidos = data.frame(cbind(kp_svm_10_all, kp_gbm_10_all, kp_rf_10_all, kp_svm_all, kp_gbm_all, kp_rf_all, kp_svm_all_imp, kp_gbm_all_imp, kp_rf_all_imp))
+colnames(unidos) = c("svm_reduced", "gbm_reduced", "rf_reduced", "svm_nadeleted", "gbm_nadeleted", "rf_nadeleted", 
+                     "svm_naimputed", "gbm_naimputed", "rf_naimputed")  
+#resultados formato tidy----
+resultados_kp_tidy = data.frame(rbind(kp_svm_10_sep, kp_gbm_10_sep, kp_rf_10_sep, kp_svm_all_sep, kp_svm_all_sep, kp_gbm_all_sep, kp_rf_all_sep,
+                                      kp_svm_10_all, kp_gbm_10_all, kp_rf_10_all, kp_svm_all, kp_gbm_all, kp_rf_all, kp_svm_all_imp,
+                                      kp_gbm_all_imp, kp_rf_all_imp)) 
+colnames(resultados_kp_tidy) = c("model", "kappa", "data", "set")
+#graficos de resultados----
+#datos reducidos, kappa de modelos comparando lances unidos y separados
+gkp1 <- resultados_kp_tidy %>%
+  filter(data == "reduced") %>%
+  ggplot(aes(x = model, y = as.numeric(kappa), fill = set)) +
+  geom_boxplot(alpha = 0.7) +
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="Reduced data",
+       x="Method",
+       y="kappa",
+       caption="Fuente: ")
+gkp1 + scale_fill_manual(values= c("#999999", "#E69F00"))
+
+#datos completos, kappa de modelos comparando lances unidos (eliminando NA y importando NA) y lances separados.
+gkp2 <- resultados_kp_tidy %>%
+  filter(data == "complete") %>%
+  ggplot(aes(x = model, y = as.numeric(kappa), fill = set)) +
+  geom_boxplot(alpha = 0.7) +
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="Completed data",
+       x="Method",
+       y="kappa",
+       caption="Fuente: ")
+gkp2 + scale_fill_manual(values= c("#999999", "#E69F00", "#56B4E9"), 
+                         labels=c("separated", "united, importing NA values", "united, removing NA values"))
+
+
+#datos unidos (eliminando NA), kappa de modelos comparando datos reducidos y completos
+gkp3 <- resultados_kp_tidy %>%
+  filter(set == c("united_na_rm", "united")) %>%
+  ggplot(aes(x = model, y = as.numeric(kappa), fill = data)) +
+  geom_boxplot(alpha = 0.7) +
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="United data, removing NA values",
+       x="Method",
+       y="kappa")
+gkp3 + scale_fill_manual(values= c("#999999", "#E69F00"))
+
+#datos unidos (importando NA), kappa de modelos comparando datos reducidos y completos
+gkp4 <- resultados_kp_tidy %>%
+  filter(set == c("united_na_imp", "united")) %>%
+  ggplot(aes(x = model, y = as.numeric(kappa), fill = data)) +
+  geom_boxplot(alpha = 0.7) +
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="United data, removing NA values",
+       x="Method",
+       y="kappa")
+gkp4 + scale_fill_manual(values= c("#999999", "#E69F00"))
+
+#datos separados, kappa de modelos comparando datos reducidos y completos
+gkp5 <- resultados_kp_tidy %>%
+  filter(set == c("separated")) %>%
+  ggplot(aes(x = model, y = as.numeric(kappa), fill = data)) +
+  geom_boxplot(alpha = 0.7) +
+  theme_minimal() +
+  theme(plot.title=element_text(hjust=0.5), axis.line = element_line(colour="black"))+
+  labs(title="United data, removing NA values",
+       x="Method",
+       y="kappa")
+gkp5 + scale_fill_manual(values= c("#999999", "#E69F00"))
 
 
 
@@ -65,6 +263,3 @@ colnames(unidos) = c("svm_reduced", "gbm_reduced", "rf_reduced", "svm_nadeleted"
 
 
 
-
-
-  
